65cf75ef4faeabc96ea26d2fcd10a538118a0080
Anna Wyss <annawyss@hotmail.com>
2018-11-21 18:41:18 +0100

    Standard View changed

--- app/project/api/standards/views.py
+++ app/project/api/standards/views.py
@@ -1,20 +1,12 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from project.api.models import SecurityStandard
 from project.api.standards.serializer import StandardSerializer
 
 
-# class ListStandardsView(ListAPIView):
-#     queryset = SecurityStandard.objects.all()
-#     serializer_class = StandardSerializer
-
-
-class StandardsView(GenericAPIView):
-
-    def get(self, request):
-        standard_lists = SecurityStandard.objects.all()
-        serializer = StandardSerializer(standard_lists, many=True)
-        return Response(serializer.data)
+class StandardsView(ListAPIView):
+    queryset = SecurityStandard.objects.all()
+    serializer_class = StandardSerializer
 
     def post(self, request, **kwargs):
         serializer = StandardSerializer(data=request.data, context={'request': request})
