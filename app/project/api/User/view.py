5e38bf739353cf428d2908eb48850700f105ab45
Anna Wyss <annawyss@hotmail.com>
2018-11-26 15:08:50 +0100

    lints response_helper and user

--- app/project/api/User/view.py
+++ app/project/api/User/view.py
@@ -1,5 +1,16 @@
-# from django.contrib.auth.models import User
-#
-# # Create user and save to the database
-# user = User.objects.create_user('username', 'password', 'email', 'clients')
-#
+from django.contrib.auth.models import User
+from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+
+from project.api.user.serializer import UserSerializer
+
+
+class UserListView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, **kwargs):
+        serializer = UserSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            new_user = serializer.save(**serializer.validated_data)
+            return Response(UserSerializer(new_user).data)
