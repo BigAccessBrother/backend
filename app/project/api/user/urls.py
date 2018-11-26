078fb633fa4e3d5de84af524e490d774c55a42b9
Anna Wyss <annawyss@hotmail.com>
2018-11-26 17:26:18 +0100

    users update and delete

--- app/project/api/user/urls.py
+++ app/project/api/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from project.api.user.view import UserListView
+from project.api.user.view import UserListView, UserDeleteView, UserUpdateView
 
 urlpatterns = [
-    path('', UserListView.as_view(), name='new_standards'),
+    path('', UserListView.as_view(), name='list_users'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='delete_user'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='update_user'),
 ]
--- app/project/api/user/view.py
+++ app/project/api/user/view.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -17,3 +17,23 @@ class UserListView(ListAPIView):
         if serializer.is_valid(raise_exception=True):
             new_user = serializer.save(**serializer.validated_data)
             return Response(UserSerializer(new_user).data)
+
+
+class UserDeleteView(DestroyAPIView):
+    permission_classes = (IsAdmin, IsAuthenticated)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateView(UpdateAPIView):
+    permission_classes = (IsAdmin, IsAuthenticated)
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.kwargs['pk'])
+
+    def partial_update(self, request, *args, **kwargs):
+        serializer = UserSerializer(data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save(**serializer.validated_data)
+            return Response(UserSerializer(user).data)
