9d0296c1c82c58485279204a4c3466a25a3cf16f
Anna Wyss <annawyss@hotmail.com>
2018-11-26 14:59:16 +0100

    list and create users

--- app/project/api/user/view.py
+++ app/project/api/user/view.py
@@ -1,8 +1,16 @@
-# from django.contrib.auth.models import user
-#
-# # Create user and save to the database
-# user = user.objects.create_user('username', 'password', 'email')
-#
-# permission_classes = [
-#         IsAuthenticated,
-#         IsOwnerOrReadOnly
+from django.contrib.auth.models import User
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+
+from project.api.user.serializer import UserSerializer
+
+
+class UserListView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def post(self, request, **kwargs):
+        serializer = UserSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid(raise_exception=True):
+            new_user = serializer.save(**serializer.validated_data)
+            return Response(UserSerializer(new_user).data)
