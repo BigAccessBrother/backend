e5985c351867f57bf5a29e57a5915843614bed20
Anna Wyss <annawyss@hotmail.com>
2018-11-28 16:58:51 +0100

    try to build with the same code

--- app/project/api/user/view.py
+++ app/project/api/user/view.py
@@ -1,22 +1,11 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import ListAPIView, DestroyAPIView, UpdateAPIView, GenericAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
-from project.api.permissions import IsAdmin
-from project.api.user.serializer import UserSerializer, ActiveUserSerializer
-
-
-class ActiveUserView(GenericAPIView):
-    queryset = User.objects.all()
-    serializer_class = ActiveUserSerializer
-
-    def get(self, *args, **kwargs):
-        return Response(UserSerializer(self.request.user).data)
+from project.api.user.serializer import UserSerializer
 
 
 class UserListView(ListAPIView):
-    permission_classes = (IsAdmin, IsAuthenticated)
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
@@ -25,23 +14,3 @@ class UserListView(ListAPIView):
         if serializer.is_valid(raise_exception=True):
             new_user = serializer.save(**serializer.validated_data)
             return Response(UserSerializer(new_user).data)
-
-
-class UserDeleteView(DestroyAPIView):
-    permission_classes = (IsAdmin, IsAuthenticated)
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserUpdateView(UpdateAPIView):
-    permission_classes = (IsAdmin, IsAuthenticated)
-    serializer_class = UserSerializer
-
-    def get_queryset(self):
-        return User.objects.filter(id=self.kwargs['pk'])
-
-    def partial_update(self, request, *args, **kwargs):
-        serializer = UserSerializer(data=request.data, partial=True)
-        if serializer.is_valid(raise_exception=True):
-            user = serializer.save(**serializer.validated_data)
-            return Response(UserSerializer(user).data)
