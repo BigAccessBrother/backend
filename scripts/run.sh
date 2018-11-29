79002392d9954884562fdf980a362f084cffcffe
Anna Wyss <annawyss@hotmail.com>
2018-11-29 14:34:18 +0100

    no makemigrations in run.sh script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:8000
 #exec /opt/conda/envs/app/bin/uwsgi --ini /scripts/uwsgi.ini
