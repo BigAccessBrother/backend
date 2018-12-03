2731fbf27b82a1d9fa1b74892411971e337787db
pascal_rueegger <pascalrueegger@gmx.ch>
2018-12-03 12:10:59 +0100

    up- and downloading agent installers

--- app/project/api/agent/urls.py
+++ app/project/api/agent/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from project.api.agent.views import ListAllAgents, AgentDeleteView, AgentRegisterView, AgentActivateView
+from project.api.agent.views import ListAllAgents, AgentDeleteView, \
+        AgentRegisterView, AgentActivateView, AgenInstallerDownloadView
 
 urlpatterns = [
         path('', ListAllAgents.as_view(), name='list_all_agents'),
         path('<int:pk>/delete/', AgentDeleteView.as_view(), name='delete_agent'),
         path('<int:pk>/activate/', AgentActivateView.as_view(), name='activate_agent'),
         path('register/', AgentRegisterView.as_view(), name='register_agent'),
+        path('installer/', AgenInstallerDownloadView.as_view(), name='download-installer'),
 ]
