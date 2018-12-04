c8270761debf94d3e013dc7b2e0939144c51b17c
pascal_rueegger <pascalrueegger@gmx.ch>
2018-12-04 17:58:05 +0100

    added check for system_serial_number on agent creation. updated agent naming. updated user creation

--- app/project/api/agent/agent_helper.py
+++ app/project/api/agent/agent_helper.py
@@ -1,10 +1,3 @@
 def name_agent(email, count):
-    name = email
-    machine_count = ''
-    if count == 1:
-        machine_count = ', 2nd computer'
-    elif count == 2:
-        machine_count = ', 3rd computer'
-    elif count > 2:
-        machine_count = f', {count}th computer'
-    return name + machine_count
+    name = email.split('@')[0]
+    return f'{name} #{count + 1}' if count else name
