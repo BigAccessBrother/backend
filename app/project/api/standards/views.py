1ca115a052995a5266352f726e643a6fe00abd3d
pascal_rueegger <pascalrueegger@gmx.ch>
2018-12-11 16:03:43 +0100

    clean standards, update models

--- app/project/api/standards/views.py
+++ app/project/api/standards/views.py
@@ -1,18 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from project.api.models import SecurityStandard
 from project.api.permissions import IsAdmin
 from project.api.standards.serializer import StandardSerializer
 
 
-class StandardsView(ListAPIView):
+class StandardsView(ListCreateAPIView):
+    """List all security standards or create a new one"""
     permission_classes = (IsAdmin, IsAuthenticated)
     queryset = SecurityStandard.objects.all()
     serializer_class = StandardSerializer
-
-    def post(self, request, **kwargs):
-        serializer = StandardSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid(raise_exception=True):
-            standard_list = serializer.save(**serializer.validated_data)
-            return Response(StandardSerializer(standard_list).data)
